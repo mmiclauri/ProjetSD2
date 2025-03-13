d860f812340b899bb35d1a0a908701ed145cee90
Sikawie <mariam.miclauri@student.vinci.be>
2025-03-13 11:14:48 +0100

    Initial commit

---
+++ src/Graph.java
@@ -0,0 +1,10 @@
+public class Graph {
+    public Graph(String file, String file1) {
+    }
+
+    public void trouverCheminLePlusCourt(String artistA, String artistB) {
+    }
+
+    public void trouverCheminMaxMentions(String artistA, String artistB) {
+    }
+}
